ad649cf6da5e44f2acceb7cc8a3cdf18306928e3
Jean Loaiza <jc.loaiza89@gmail.com>
2022-04-25 20:24:12 -0500

    Crud

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -231,7 +231,7 @@ const Main = () => {
                                     </tbody>
                                 </table>
 
-                                
+
                             ))
                         }
                     </ul>
